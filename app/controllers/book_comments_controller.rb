2d2bd81199690b5c6745b265b7834fb362af653f
Alexander4649 <105344687+Alexander4649@users.noreply.github.com>
2022-06-18 09:07:12 +0000

    [Add]非同期通信処理完了!

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,16 +1,23 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to request.referer
-    #redirect_back(fallback_location: root_path)
+    @book = Book.find(params[:book_id])
+    @book_comment = current_user.book_comments.new(book_comment_params)
+    @book_comment.book_id = @book.id
+    @book_comment.save
+    #render :book_comments  #render先にjsファイルを指定
   end
+    #@book_comment.save
+    #@book = Book.find(params[:id])
+    #@book_comment = BookComment.new
+    #redirect_to request.referer
+    #redirect_back(fallback_location: root_path)
 
   def destroy
     BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to request.referer
+    #redirect_to request.referer
+    @book = Book.find(params[:book_id])  
+    #render :book_comments
+    #render :destroy
   end
 
   private
